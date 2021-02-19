04b25008207068756b05210651cfb9330a436d0b
YasunobuSasaki <yasunobu.sasaki@gmail.com>
2021-02-20 02:55:47 +0900

    ivs playerで再生する処理完了

--- ios/Classes/SwiftIvsPlayerPlugin.swift
+++ ios/Classes/SwiftIvsPlayerPlugin.swift
@@ -8,11 +8,56 @@ public class SwiftIvsPlayerPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "ivs_player", binaryMessenger: registrar.messenger())
     let instance = SwiftIvsPlayerPlugin()  
+    registrar.register(instance, withId: "ivs_player/player_View")
     registrar.addMethodCallDelegate(instance, channel: channel)
+     // 登録
   }
 
+
+    private let playerView = IVSPlayerView()
+
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    
     result("iOS " + UIDevice.current.systemVersion)
   }
+
+}
+
+
+extension SwiftIvsPlayerPlugin: FlutterPlatformViewFactory {
+  public func create(withFrame frame: CGRect, viewIdentifier viewId: Int64, arguments args: Any?) -> FlutterPlatformView {
+
+     let player = IVSPlayer()
+     playerView.player = player
+    if let args = args as? [String:Any?] {
+        let urlString = args["url"] as? String ?? "no url"
+        let url = URL(string: urlString)!;
+        player.load(url)
+        player.play()
+        
+        let isHidden = args["isHidden"] as? Bool ?? false;
+        if (isHidden) {
+            playerView.isHidden = true;
+        }
+        
+     }
+     
+     return SwiftIvsyPlatformView(playerView)
+  }
+    public func createArgsCodec() -> FlutterMessageCodec & NSObjectProtocol {
+       return FlutterStandardMessageCodec.sharedInstance()
+  }
+}
+
+
+class SwiftIvsyPlatformView: NSObject, FlutterPlatformView {
+    let playerView: IVSPlayerView
+    init(_ playerView: IVSPlayerView) {
+        self.playerView = playerView
+        super.init()
+    }
+
+    func view() -> UIView {
+        return playerView
+    }
 }
--- lib/ivs_player.dart
+++ lib/ivs_player.dart
@@ -1,12 +1,32 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 class IvsPlayer {
   static const MethodChannel _channel = const MethodChannel('ivs_player');
 
-  static Future<String> get platformVersion async {
+/*  static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }*/
+
+  static UiKitView player({String url, bool isHidden = false}) {
+    UiKitView res;
+    try {
+      res = UiKitView(
+          viewType: 'ivs_player/player_View',
+          creationParams: <String, dynamic>{
+            'url': url,
+            'isHidden': isHidden,
+          },
+          creationParamsCodec: StandardMessageCodec(),
+          onPlatformViewCreated: (id) {
+            print('UiKitView created: id = $id');
+          });
+    } catch (e) {
+      print(e);
+    }
+    return res;
   }
 }
